0ad1a588aa716ebad739ed3e42dac60ae9c3567d
ThiagoF <thiago.favretto1@gmail.com>
2025-09-11 12:14:57 -0300

    P1, P2, Enemys

--- code/Const.py
+++ code/Const.py
@@ -1,14 +1,20 @@
+import pygame
+
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
 
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
+
+
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT',)
+
+EVENT_ENEMY = pygame.USEREVENT + 1
 
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -18,6 +24,22 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
-
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 3,
+    'Enemy2': 2
 }
+
+SPAWN_TIME = 4000
+
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                    'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                    'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                    'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
 
